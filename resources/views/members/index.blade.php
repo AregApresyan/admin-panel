857c56f3c42f515452ede1c75615a2ac3e5b1b39
ManvelY <manvel.y.y@tumo.org>
2024-07-20 13:15:55 +0400

    Lesson 5 Datatables

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -1,38 +1,55 @@
 @extends('layouts.app')
 
+@section('head')
+ <style>
+        .delete-button{
+            margin-left: 10px;
+        }
+    </style>
+ @endsection
 @section('content')
-<div class="container">
-    
-    <table>
-        <tr>
-            <th>Id</th>
-            <th>Email</th>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Gender</th>
-            <th>Age</th>
-            <th>Job Title</th>
-            <th>Salery</th>
-            <th>Joined At</th>
-        </tr>
-        @foreach ($members as $member)
-        <tr>
-            <td>{{$member->id}}</td>
-            <td>{{$member->email}}</td>
-            <td>{{$member->eirst_name}}</td>
-            <td>{{$member->last_name}}</td>
-            <td>{{$member->gender}}</td>
-            <td>{{$member->age}}</td>
-            <td>{{$member->position}}</td>
-            <td>{{$member->sallery}}</td>
-            <td>{{$member->joined_at}}</td>
-            
-
-
-        </tr>
-            
-        @endforeach
-    </table>
-
-</div>
+    <div class="container">
+        <table id="members_list_table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Email</th>
+                    <th>First name</th>
+                    <th>Last name</th>
+                    <th>Gender</th>
+                    <th>Age</th>
+                    <th>Job Title</th>
+                    <th>Salery</th>
+                    <th>Joined At</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($members as $member)
+                <tr>
+                    <td>{{$member->id}}</td>
+                    <td>{{$member->email}}</td>
+                    <td>{{$member->eirst_name}}</td>
+                    <td>{{$member->last_name}}</td>
+                    <td>{{$member->gender}}</td>
+                    <td>{{$member->age}}</td>
+                    <td>{{$member->position}}</td>
+                    <td>{{$member->sallery}}</td>
+                    <td>{{ Carbon\Carbon::parse($member->joined_at)->format('d-m-Y') }}</td>
+                    <td>
+                        <a class="btn btn-sm btn-primary" href="">Edit</a>
+                        <a class="btn btn-sm btn-danger delete-button" href="">Delete</a>
+                    </td>
+                </tr>   
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <script>
+        $(document).ready(()=>{
+             $('#members_list_table').DataTable();
+        })
+       
+    </script>
+   
 @endsection
